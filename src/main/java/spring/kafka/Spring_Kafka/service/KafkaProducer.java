1b51837b6e981581fac9b18adf0324e208e2fbb9
viraj-vvk <viraj.vvk@gmail.com>
2024-12-09 18:33:31 +0530

    Implemented KafkaProducer

---
+++ src/main/java/spring/kafka/Spring_Kafka/service/KafkaProducer.java
@@ -0,0 +1,24 @@
+package spring.kafka.Spring_Kafka.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        LOGGER.info("Message sent: {}", message);
+        kafkaTemplate.send("sampleTopic", message);
+    }
+}
