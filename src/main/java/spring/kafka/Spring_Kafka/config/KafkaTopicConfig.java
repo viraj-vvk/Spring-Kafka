5adc0a9f0d853d7fcf045bc3dbf49c25551fa892
viraj-vvk <viraj.vvk@gmail.com>
2024-12-09 18:11:08 +0530

    Implemented SampleTopic (1st Kafka topic)

---
+++ src/main/java/spring/kafka/Spring_Kafka/config/KafkaTopicConfig.java
@@ -0,0 +1,15 @@
+package spring.kafka.Spring_Kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic sampleTopic() {
+        return TopicBuilder.name("sampleTopic").build();
+    }
+}
